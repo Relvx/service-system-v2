285609424d7ea112fb324cf5b747c0ef693733c8
Andrey Kalashnikov <lolishe303@gmail.com>
2026-02-25 10:10:29 +0300

    feat: models repr

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,4 +9,9 @@ AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass
+    def __repr__(self) -> str:
+        cols = []
+        for i, col in enumerate(self.__table__.columns.keys()):
+            cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__table__}: {', '.join(cols)}>"
